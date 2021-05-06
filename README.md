d0b86cae5cf9b297ede83fd85f274cf50e3aa236
Jk8c19 <jake.cooper08@gmail.com>
2021-05-06 20:11:41 +0100

    Add Nextcloud WebDAV Upload Function

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # the_collector
-Grabs imgur and i.reddit images from subreddits, then posts them to discord via a webhook uri.
+Grabs imgur and i.reddit images from subreddits, then posts them to discord via a webhook uri and/or pass it WebDAV details to upload the files to a folder.
 
 ```
 docker run \
   --name=the_collector \
   --rm \
-  -e subreddit=""\
-  -e webhook_uri=""\
-  -e post_qty=1\
+  -e subreddit="" \
+  -e webhook_uri="" \
+  -e post_qty=1 \
   -e logging_level="info" \
-  --restart unless-stopped \
+  -e webdav_uri="" \
+  -e webdav_user="" \
+  -e webdav_pass="" \
   jk8c19/the_collector:latest
 ```
 
 This is a run once container, so using --rm will clean up once it's done its thing.
+
+WebDAV upload was intended for use with Nextcloud and is only tested for this application.
 
 # Parameters
 
 | Parameter | Function | Req | Default |
 |-|-|-|-|
 | subreddit | name of a reddit subreddit | Y
-| webhook_uri | full uri to discord webhook | Y
+| webhook_uri | full uri to discord webhook | N
 | post_qty | amount of posts to gather | N | 1 |
 | logging_level | takes in logging level | N | info |
+| webdav_uri | path to put image to | N
+| webdav_user | webdav username | N
+| webdav_pass | webdav application password | N
--- collector.py
+++ collector.py
@@ -8,13 +8,21 @@ import logging
 import re
 from time import sleep
 from os import environ
+from os import remove
 
-subreddit = environ['subreddit']
-webhook_uri = environ['webhook_uri']
-post_qty = int(environ['post_qty'])
 logging_level = getattr(logging, environ['logging_level'].upper())
 
-def search_subreddit(subreddit, qty):
+subreddit = environ['subreddit']
+post_qty = int(environ['post_qty'])
+
+webhook_uri = environ.get('webhook_uri')
+
+webdav_uri = environ.get('webdav_uri')
+webdav_user = environ.get('webdav_user')
+webdav_pass = environ.get('webdav_pass')
+
+
+def search_subreddit(qty):
     feed = feedparser.parse('https://reddit.com/r/'+subreddit+'.rss')
     entries = []
     for x in range(post_qty+2):
@@ -26,9 +34,7 @@ def search_subreddit(subreddit, qty):
     return entries
 
 def post_discord_message(webhook_uri, message):
-    data = {
-        "content": message
-    }
+    data = {"content": message}
 
     result = requests.post(webhook_uri, json=data)
 
@@ -37,20 +43,73 @@ def post_discord_message(webhook_uri, message):
     except requests.exceptions.HTTPError as err:
         logging.warning(err)
     else:
-        logging.info("Successfully shitposted m'lord")
+        logging.info("Successfully shitposted!")
         logging.debug(data)
-        
+
+def upload_webdav(file_uri, subreddit):
+
+    file_name = re.findall(r"https:\/\/.*\/([a-zA-Z0-9.]*)", file_uri)
+    file_type = re.findall(r"https:\/\/.*\/.*\.(.*)", file_uri)
+    logging.debug(f"From {file_uri} found {file_name[0]}")
+    result = requests.get(file_uri)
+
+    file = open(file_name[0], "wb")
+    file.write(result.content)
+    file.close()
+
+    if file_type[0] == "jpg":
+        content_type = "image/jpeg"
+    elif file_type[0] == "png":
+        content_type = "image/png"
+    else:
+        logging.info("No clue what this filetype is, eject!")
+        sys.exit()
+
+    uri = f"{webdav_uri}/{file_name[0]}"
+    logging.debug(uri)
+
+    header = {"content-type": content_type}
+    result = requests.put(uri, data=open(file_name[0], "rb"), headers=header, auth=(webdav_user, webdav_pass))
+
+    try:
+        result.raise_for_status()
+        logging.info(f"Upload of {file_name[0]} complete")
+    except requests.exceptions.HTTPError as err:
+        logging.warning(err)
+    
+    remove(file_name[0])
+    logging.debug(f"Deleted {file_name[0]}")
+
+if webdav_uri == None:
+    webdav = False
+else:
+    webdav = True
+
+if webhook_uri == None:
+    webhook = False
+else:
+    webhook = True
 
 logging.basicConfig(stream=sys.stdout, level=logging_level)
 logging.info("Starting the_collector")
 logging.debug("\t- Subreddit: {}".format(subreddit))
-logging.debug("\t- Webhook URI: {}".format(webhook_uri))
 logging.debug("\t- Quantity: {}".format(post_qty))
 logging.debug("\t- Logging level: {}".format(environ['logging_level']))
+if webhook == True:
+    logging.debug("\t- Webhook URI: {}".format(webhook_uri))
+if webdav == True:
+    logging.debug("\t- WebDAV URI: {}".format(webdav_uri))
+    logging.debug("\t- WebDAV User: {}".format(webdav_user))
+    logging.debug("\t- WebDAV Pass: {}".format(webdav_pass))
 
-results = search_subreddit(subreddit,post_qty)
+results = search_subreddit(post_qty)
 logging.info(f"Collected {len(results)} image links")
 for post in results:
-    post_discord_message(webhook_uri, post)
-    sleep(1)
+    if webhook == True:
+        post_discord_message(webhook_uri, post)
+        sleep(1)
+    
+    if webdav == True:
+        upload_webdav(post, subreddit)
+
 logging.info("We're done here")
